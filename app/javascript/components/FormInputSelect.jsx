1408f7aa459bd38ac5e353555c064004a9091ff7
Lee Atchison <lee@leeatchison.com>
2025-11-21 08:22:52 -0800

    Added FormInputSelect.

---
+++ app/javascript/components/FormInputSelect.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const FormInputSelect = ({fieldId,fieldName,fieldValue,fieldOptions,label,hint,handleChange,handleBlur,errorMsg}) => {
+    return (
+        <>
+            <div
+                className={errorMsg && "field_with_errors"}
+            >
+                {label && <label htmlFor={fieldId}>{label}:</label>}
+                <select
+                    id={fieldId}
+                    name={fieldName}
+                    value={fieldValue}
+                    onValueUpdate={handleChange}
+                    onBlur={handleBlur}
+                >
+                    {fieldOptions.map((option) => (
+                        <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+                {hint && <span className="hint">{hint}</span>}
+                {errorMsg && <span className="error">{errorMsg}</span>}
+            </div>
+        </>
+    )
+}
